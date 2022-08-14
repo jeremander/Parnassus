21250d949f78343fa65148e6af2fd4ba3d075ba6
Jeremy Silver <jeremys@nessiness.com>
2022-08-14 13:31:15 -0400

    Added conversion from MIDI/WAV to MP3.

--- parnassus/Command/Convert.hs
+++ parnassus/Command/Convert.hs
@@ -2,12 +2,16 @@
 
 module Command.Convert where
 
+import Control.Monad (unless)
 import Data.Char (toLower)
+import Data.Default (Default(..))
 import Options.Applicative
+import System.Directory (doesFileExist)
 import System.IO.Error (ioError, userError)
 import System.FilePath.Posix (takeExtension)
 
 import Music.Convert
+import Synth (MidiMp3Opts(..), MidiWavOpts(..), runMidiToMp3, runMidiToWav, runWavToMp3, SynthOpts(..), WavMp3Opts(..))
 
 
 data ConvertOpts = ConvertOpts {
@@ -22,21 +26,56 @@ convertOptsParser = helper <*> (ConvertOpts <$>
     <*> argument str (metavar "OUTFILE" <> help "(required) output file")
     <*> optional (strOption (long "sound-font" <> short 's' <> metavar "SOUNDFONT" <> help "SoundFont (.sf2) file")))
 
+type Converter = ConvertOpts -> IO ()
+
+mkConv :: (FilePath -> FilePath -> IO ()) -> Converter
+mkConv func opts = func (inputFile opts) (outputFile opts)
+
+runConv :: ConvertOpts -> Converter -> IO ()
+runConv opts conv = do
+    putStrLn $ "Converting " ++ inputFile opts ++ " to " ++ outputFile opts ++ "..."
+    conv opts
+    putStrLn "Done!"
+
+convMidiToWav :: Converter
+convMidiToWav (ConvertOpts {inputFile, outputFile, soundFont}) = runMidiToWav midiWavOpts
+    where
+        synthOpts = SynthOpts (Just inputFile) soundFont
+        midiWavOpts = MidiWavOpts synthOpts outputFile
+
+convWavToMp3 :: Converter
+convWavToMp3 (ConvertOpts {inputFile, outputFile}) = runWavToMp3 wavMp3Opts
+    where
+        wavMp3Opts = WavMp3Opts {wavPath = inputFile, mp3Path = outputFile, mp3Params = def}
+
+convMidiToMp3 :: Converter
+convMidiToMp3 (ConvertOpts {inputFile, outputFile, soundFont}) = runMidiToMp3 midiMp3Opts
+    where
+        synthOpts = SynthOpts (Just inputFile) soundFont
+        mp3Params = def
+        midiMp3Opts = MidiMp3Opts synthOpts mp3Params outputFile
+
 runConvert :: ConvertOpts -> IO ()
-runConvert (ConvertOpts {inputFile, outputFile, soundFont}) = do
-    let inputExt = toLower <$> takeExtension inputFile
-    let outputExt = toLower <$> takeExtension outputFile
-    let runConv conv = do
-        putStrLn $ "Converting " ++ inputFile ++ " to " ++ outputFile ++ "..."
-        fail "not yet implemented"
-        -- conv inputFile outputFile
-        putStrLn "Done!"
-    case inputExt of
-        ".ly" -> case outputExt of
-            ".mid" -> runConv lilypondToMidi
-            _      -> fail $ "invalid output extension " ++ takeExtension outputFile
-        ".mid" -> case outputExt of
-            ".ly" -> runConv midiToLilypond
-            _     -> fail $ "invalid output extension " ++ takeExtension outputFile
-        _     -> fail $ "invalid input extension " ++ takeExtension inputFile
+runConvert opts@(ConvertOpts {inputFile, outputFile}) = do
+    let inputExt = takeExtension inputFile
+    let outputExt = takeExtension outputFile
+    let inputExt' = toLower <$> inputExt
+    let outputExt' = toLower <$> outputExt
+    let invalidInputExt = fail $ "invalid input extension " ++ inputExt
+    let invalidOutputExt = fail $ "invalid output extension " ++ outputExt
+    let runConv' = runConv opts
+    inputExists <- doesFileExist inputFile
+    unless inputExists $ fail $ "file " ++ show inputFile ++ " does not exist"
+    case inputExt' of
+        ".ly" -> case outputExt' of
+            ".mid" -> runConv' $ mkConv lilypondToMidi
+            _      -> invalidOutputExt
+        ".mid" -> case outputExt' of
+            ".ly"  -> runConv' $ mkConv midiToLilypond
+            ".mp3" -> runConv' convMidiToMp3
+            ".wav" -> runConv' convMidiToWav
+            _      -> invalidOutputExt
+        ".wav" -> case outputExt' of
+            ".mp3" -> runConv' convWavToMp3
+        _     -> invalidInputExt
     return ()
--- parnassus/Synth.hs
+++ parnassus/Synth.hs
@@ -2,19 +2,39 @@
 
 module Synth where
 
-import Data.Maybe (catMaybes)
+import Data.Default (Default(..))
+import Data.Maybe (catMaybes, fromJust)
 import Options.Applicative
 import System.Exit (ExitCode(..))
+import System.FilePath.Posix (takeBaseName)
+import System.IO.Temp (withTempFile)
 import System.Process (callProcess, readProcessWithExitCode)
 
 
 type Args = [String]
 
--- | Checks whether the @fluidsynth@ executable is present on the user's path.
-checkFluidsynth :: IO Bool
-checkFluidsynth = do
-    (code, _, _) <- readProcessWithExitCode "which" ["fluidsynth"] ""
+-- | Checks whether some executable is present on the user's path.
+checkProgram :: FilePath -> IO Bool
+checkProgram prog = do
+    (code, _, _) <- readProcessWithExitCode "which" [prog] ""
     return $ code == ExitSuccess
+
+-- | Runs a program and arguments, first checking if the program exists on the user's path.
+--   Prints out the command-line arguments before executing it.
+runProgram :: FilePath -> Args -> IO ()
+runProgram prog args = do
+    hasProg <- checkProgram prog
+    putStrLn cmd
+    if hasProg
+        then callProcess prog args
+        else fail $ "could not find " ++ prog ++ " executable on PATH"
+    return ()
+    where cmd = prog ++ " " ++ unwords args
+
+-- ** FluidSynth
+
+runFluidsynthCmd :: Args -> IO ()
+runFluidsynthCmd = runProgram "fluidsynth"
 
 data SynthOpts = SynthOpts {
     midiPath :: Maybe FilePath,  -- MIDI file to synthesize
@@ -28,11 +48,14 @@ synthOptsParser midiRequired = helper <*> (SynthOpts <$> parseMidiPath <*> parse
         parseMidiPath = reqMod $ strArgument (metavar "MIDI_FILE" <> help "input MIDI file to play")
         parseSoundFontPath = optional $ strOption (long "sound-font" <> short 's' <> metavar "SOUND_FONT" <> help "SoundFont file")
 
+-- | Converts 'SynthOpts' to @fluidsynth@ command-line arguments.
 synthOptsArgs :: SynthOpts -> Args
 synthOptsArgs (SynthOpts {midiPath, soundFontPath}) = concat $ catMaybes [soundFontArg, midiArg]
     where
         soundFontArg = pure <$> soundFontPath
         midiArg = pure <$> midiPath
+
+-- ** Playback
 
 data PlayOpts = PlayOpts {
     synthOpts :: SynthOpts,
@@ -54,13 +77,57 @@ playOptsArgs (PlayOpts {synthOpts, portName, noShell}) = concat (catMaybes [port
         noShellArg = if noShell then Just ["--no-shell"] else Nothing
 
 runPlay :: PlayOpts -> IO ()
-runPlay playOpts = do
-    hasFluidsynth <- checkFluidsynth
-    putStrLn cmd
-    if hasFluidsynth
-        then callProcess "fluidsynth" args
-        else fail "could not fluidsynth executable on PATH"
-    return ()
+runPlay = runFluidsynthCmd . playOptsArgs
+
+-- ** MIDI to WAV
+
+data MidiWavOpts = MidiWavOpts SynthOpts FilePath
+
+-- | Converts 'WavOpts' to @fluidsynth@ command-line arguments.
+midiWavOptsArgs :: MidiWavOpts -> Args
+midiWavOptsArgs (MidiWavOpts synthOpts wavPath) = synthOptsArgs synthOpts ++ ["-F", wavPath]
+
+runMidiToWav :: MidiWavOpts -> IO ()
+runMidiToWav = runFluidsynthCmd . midiWavOptsArgs
+
+-- ** WAV to MP3
+
+data Mp3Params = Mp3Params {
+    sampleFreq :: Int,  -- sample frequency (samples per second)
+    audioChannels :: Int,  -- number of audio channels (1 = mono, 2 = stereo)
+    bitrate :: Int,  -- bit rate (kilobits per second)
+    overwrite :: Bool  -- overwrite output file if it exists
+}
+
+instance Default Mp3Params where
+    def = Mp3Params {sampleFreq = 44100, audioChannels = 2, bitrate = 192, overwrite = True}
+
+mp3ParamsArgs :: Mp3Params -> Args
+mp3ParamsArgs (Mp3Params {sampleFreq, audioChannels, bitrate, overwrite}) = ["-vn", "-ar"] ++ [show sampleFreq] ++ ["-ac"] ++ [show audioChannels] ++ ["-b:a"] ++ [show bitrate ++ "k"] ++ ["-y" | overwrite]
+
+data WavMp3Opts = WavMp3Opts {
+    wavPath :: FilePath,
+    mp3Path :: FilePath,
+    mp3Params :: Mp3Params
+}
+
+wavMp3OptsArgs :: WavMp3Opts -> Args
+wavMp3OptsArgs (WavMp3Opts {wavPath, mp3Path, mp3Params}) = ["-i", wavPath] ++ mp3ParamsArgs mp3Params ++ [mp3Path]
+
+runWavToMp3 :: WavMp3Opts -> IO ()
+runWavToMp3 = runProgram "ffmpeg" . wavMp3OptsArgs
+
+-- ** MIDI to MP3
+
+data MidiMp3Opts = MidiMp3Opts SynthOpts Mp3Params FilePath
+
+runMidiToMp3 :: MidiMp3Opts -> IO ()
+runMidiToMp3 (MidiMp3Opts synthOpts mp3Params mp3Path) = withTempFile "." template go
     where
-        args = playOptsArgs playOpts
-        cmd = "fluidsynth " ++ unwords args
+        baseName = takeBaseName $ fromJust $ midiPath synthOpts
+        template = baseName ++ ".wav"
+        go wavPath _ = do
+            let midiWavOpts = MidiWavOpts synthOpts wavPath
+            runMidiToWav midiWavOpts
+            let wavMp3Opts = WavMp3Opts wavPath mp3Path mp3Params
+            runWavToMp3 wavMp3Opts
